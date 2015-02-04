21dc130ec44a9e2feb6da46a4481d7ce8b2b5e8a
vijmanda <manda.vijay@gmail.com>
2015-02-04 11:34:49 +0530

    Unit and Functional test case for Seed App

---
+++ src/test/java/com/cisco/dft/sda/api/controller/ControllerIT.java
@@ -0,0 +1,47 @@
+package com.cisco.dft.sda.api.controller;
+
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.RestTemplate;
+
+import com.cisco.dft.sda.api.Application;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest({ "server.port=0" })
+public class ControllerIT {
+	@Value("${local.server.port}")
+	private int port;
+
+	private URL base;
+	private RestTemplate template;
+
+	@Before
+	public void setUp() throws Exception {
+		this.base = new URL("http://localhost:" + port
+				+ "/dft/sda/test/Testing");
+		template = new TestRestTemplate();
+	}
+
+	@Test
+	public void getHello() throws Exception {
+		ResponseEntity<String> response = template.getForEntity(
+				base.toString(), String.class);
+		System.out.println("BODY " + response.getBody());
+
+		//assertEquals(response.getBody(), "Testing");
+	}
+}
