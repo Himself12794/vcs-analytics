d2d5794716deeb3550968dbe3b8b19f65c3e03dc
Vivek Sethi <vivekse@cisco.com>
2015-02-13 03:19:38 +0530

    some more cleanup

--- src/main/java/com/cisco/dft/sda/api/service/TestService.java
+++ src/main/java/com/cisco/dft/sda/api/service/TestService.java
@@ -4,8 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.cisco.dft.sda.api.controller.Controller;
-
 /**
  * Implements API business logic
  * 
--- src/main/java/com/cisco/dft/sda/api/util/Constants.java
+++ src/main/java/com/cisco/dft/sda/api/util/Constants.java
@@ -13,13 +13,4 @@ public interface Constants {
 	enum STATUS {
 		SUCCESS, FAILURE, INVALID_REQUEST;
 	}
-
-	public static final String SOME_NAME = "some value";
-	public static final String QUEUE_NAME = "test";
-	public static final String TOPIC_EXCHANGE_NAME = "spring-boot-exchange";
-	public static final String USERNAME = "indrayam";
-	public static final String PASSWORD = "raju2she";
-	public static final int PORT = 5672;
-	public static final String VIRTUALHOST = "dft-dev-vhost";
-	public static final String HOSTNAME = "indrayam2.cisco.com";
 }
