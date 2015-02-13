d2b60b8af99a59b888b6090a8b5e55c33d14e294
Vivek Sethi <vivekse@cisco.com>
2015-02-14 02:47:11 +0530

    encryption of passwords in yaml file

--- src/main/java/com/cisco/dft/sda/api/util/Util.java
+++ src/main/java/com/cisco/dft/sda/api/util/Util.java
@@ -1,5 +1,9 @@
 package com.cisco.dft.sda.api.util;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Defines helper/util methods that performs common functionalities which
  * doesn't depend on the state of the object.
@@ -9,6 +13,9 @@ package com.cisco.dft.sda.api.util;
  * @date January 22, 2015
  */
 public class Util {
+	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(Util.class);
 
 	/**
 	 * Determines whether the given string is a null or an empty string
@@ -18,5 +25,26 @@ public class Util {
 	 */
 	public static Boolean isNullOrEmpty(String value) {
 		return (value == null || value.trim().length() == 0);
+	}
+	
+	/**
+	 * Decrypts the password using the Jasypt library 
+	 * @param encrypted password, key
+	 *            
+	 * @return decrypted password
+	 */
+	public static String decrypt(String password, String key) {
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		String encryptedPassword = null;
+		encryptor.setPassword(key);
+		try {
+			LOGGER.info("Decrypting the password:  " + password);
+			encryptedPassword = encryptor.decrypt(password);
+			LOGGER.info("Encrypted Password:  " + encryptedPassword);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		return encryptedPassword;
 	}
 }
