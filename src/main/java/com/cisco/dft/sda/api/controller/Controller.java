d4f34869fd5a586802e53db27f9872155d497e60
Vivek Sethi <vivekse@cisco.com>
2015-02-15 23:31:14 +0530

    adding sample methods to do a sample GET and sample POST request

--- src/main/java/com/cisco/dft/sda/api/controller/Controller.java
+++ src/main/java/com/cisco/dft/sda/api/controller/Controller.java
@@ -3,16 +3,20 @@ package com.cisco.dft.sda.api.controller;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cisco.dft.sda.api.pojo.GenericResponseObject;
+import com.cisco.dft.sda.api.pojo.GenericRequestObject;
 import com.cisco.dft.sda.api.service.TestService;
 
 /**
@@ -47,7 +51,7 @@ public class Controller {
 											// parameter with logger level info
 		LOGGER.warn("Input value:" + name); // Logs the name passed in the query
 											// parameter with logger level warn
-    /* sample error logging scenario */
+		/* sample error logging scenario */
 		try {
 			int i = 1 / 0;
 		} catch (Exception e) {
@@ -58,48 +62,49 @@ public class Controller {
 
 		return service.test(name);
 	}
-	
+
 	/**
-	 * Set the http response body with a custom json instead of sending an object
+	 * Test implementation using HTTP GET Usage:
+	 * <server-name>:<port-num>/dft/sda/get-test?param1=Hello&param2=World
 	 * 
-	 * @param response
-	 * @return void
+	 * @param request
+	 * @return
 	 */
-	@RequestMapping(value = "/dft/sda/testResponse", method = RequestMethod.GET)
-	public void testHttpResponse(HttpServletResponse response) {
-		
-		try {
-			// setting the type of the data which will be sent in http response body
-			response.setContentType("application/json");
-			
-			// setting the status code of the http response header
-			response.setStatus(200); // code can be any number or can be a predefined http status code
-			
-			// writing the data to the http response body @param : object
-			response.getWriter().print(service.getObjectJSON());
-		
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * Status code defined here for easy debugging
-	 * 
-	 * @param response
-	 * @return void
-	 */
-	@RequestMapping(value = "/dft/sda/testResponseObject", method = RequestMethod.GET)
-	public GenericResponseObject testHttpResponseObject(HttpServletResponse response) {
-		
-		// setting the type of the data which will be sent in http response body
-		response.setContentType("application/json");
-		
-		// setting the status code of the http response header
-		response.setStatus(200); // code can be any number or can be a predefined http status code
-		
-		return service.getObject();
+	@RequestMapping(value = "/dft/sda/get-test", method = RequestMethod.GET)
+	public GenericResponseObject testGet(
+			@RequestParam(value = "param1", defaultValue = "param1", required = true) String param1,
+			@RequestParam(value = "param2", defaultValue = "param2") String param2,
+			HttpServletResponse httpResponse) {
+		GenericRequestObject request = new GenericRequestObject(param1, param2);
+		return service.test(request, httpResponse);
+
+		// send a custom json instead:
+		// try {
+		// httpResponse.getWriter().print(service.getObjectJSON());
+		// } catch (IOException e) {
+		// LOGGER.error(e.getStackTrace().toString());
+		// }
 	}
 
+	/**
+	 * Test implementation using HTTP POST exchange Sample Payload: {"param1":
+	 * "Hello", "param2": "World!"}
+	 * 
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/dft/sda/post-test", method = RequestMethod.POST)
+	public GenericResponseObject testPost(
+			@RequestBody @Valid final GenericRequestObject request,
+			HttpServletResponse httpResponse) {
+		return service.test(request, httpResponse);
+
+		// send a custom json instead:
+		// try {
+		// httpResponse.getWriter().print(service.getObjectJSON());
+		// } catch (IOException e) {
+		// LOGGER.error(e.getStackTrace().toString());
+		// }
+	}
 
 }
--- src/main/java/com/cisco/dft/sda/api/service/TestService.java
+++ src/main/java/com/cisco/dft/sda/api/service/TestService.java
@@ -1,9 +1,12 @@
 package com.cisco.dft.sda.api.service;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.json.simple.JSONObject;
 import org.springframework.stereotype.Service;
 
 import com.cisco.dft.sda.api.pojo.GenericResponseObject;
+import com.cisco.dft.sda.api.pojo.GenericRequestObject;
 
 /**
  * Implements API business logic
@@ -24,22 +27,48 @@ public class TestService {
 	public String test(String name) {
 		return name;
 	}
-	
+
+	/**
+	 * an over-ridden test method with custom business logic which returns a
+	 * generic response object when called
+	 * 
+	 * @param request
+	 * @param httpResponse
+	 * @return
+	 */
+
+	public GenericResponseObject test(GenericRequestObject request,
+			HttpServletResponse httpResponse) {
+		GenericResponseObject response = new GenericResponseObject();
+		String param1 = request.getParam1();
+		String param2 = request.getParam2();
+
+		// Business logic:
+		if (param1.equals(param2)) {
+			response.setParam1("value#1");
+			response.setParam2("value#2");
+			httpResponse.setContentType("application/json");
+			httpResponse.setStatus(200); // status 200 will be returned
+		} else {
+			response.setParam1("value#3");
+			response.setParam2("value#4");
+			httpResponse.setContentType("application/json");
+			httpResponse.setStatus(400); // status 400 will be returned
+		}
+		return response;
+	}
+
+	/**
+	 * dummy method which creates and return a json
+	 * @return
+	 */
+
 	public JSONObject getObjectJSON() {
 		// dummy
 		JSONObject json = new JSONObject();
 		json.put("param1", "hello");
 		json.put("param2", "world");
-		
+
 		return json;
-	}
-	
-	public GenericResponseObject getObject() {
-		// dummy
-		GenericResponseObject objGenericResponse = new GenericResponseObject();
-		objGenericResponse.setParam1("hello");
-		objGenericResponse.setParam2("world");
-		
-		return objGenericResponse;
 	}
 }
