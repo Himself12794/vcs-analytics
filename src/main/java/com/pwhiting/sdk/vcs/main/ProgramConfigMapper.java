8471eae521bc5f0365e453752e9b86e1d76563e3
Himself12794 <pwhiting.bba@gmail.com>
2015-08-15 14:12:59 -0400

    Changed to include the use of ArgMapper interface

---
+++ src/main/java/com/pwhiting/sdk/vcs/main/ProgramConfigMapper.java
@@ -0,0 +1,14 @@
+package com.pwhiting.sdk.vcs.main;
+
+import com.pwhiting.util.ArgMapper;
+import com.pwhiting.util.ArgParser;
+
+public class ProgramConfigMapper implements ArgMapper<ProgramConfig> {
+
+	@Override
+	public ProgramConfig mapArguments(ArgParser parser) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
